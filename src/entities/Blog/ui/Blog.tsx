7466d3a32f476786c70c9c837215f5f630f21b4a
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-07-16 21:44:36 +0300

    refactor: Повысил семантику Blog

--- src/entities/Blog/ui/Blog.tsx
+++ src/entities/Blog/ui/Blog.tsx
@@ -2,28 +2,16 @@ import classes from './Blog.module.css'
 
 const Blog = () => {
   return (
-    <section>
-      <div className={classes.main_body}>
-        <div className={classes.container}>
-          <h1 className={classes.header}>Blog</h1>
-          <div className={classes.box_items}>
-            <div className={classes.item}>
-              <h2 className={classes.item_header}>UI Interactions of the week</h2>
-              <div className={classes.item_header_description}>
-                <p className={classes.item_header_description_date}>12 Feb 2019</p>
-                <p className={classes.item_header_description_separator}>|</p>
-                <p className={classes.item_header_description_name}>Express, Handlebars</p>
-              </div>
-              <p className={classes.item_description_main_text}>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
-                amet.
-              </p>
-            </div>
-            <hr className={classes.under_line} />
-          </div>
-          <div className={classes.item}>
-            <h2 className={classes.item_header}>UI Interactions of the week</h2>
+    <section className={classes.main_body}>
+      <div className={classes.container}>
+        <header className={classes.header}>
+          <h1 className={classes.header_text}>Blog</h1>
+        </header>
+        <div className={classes.box_items}>
+          <article className={classes.item}>
+            <header className={classes.item_header}>
+              <h2 className={classes.item_header_text}>UI Interactions of the week</h2>
+            </header>
             <div className={classes.item_header_description}>
               <p className={classes.item_header_description_date}>12 Feb 2019</p>
               <p className={classes.item_header_description_separator}>|</p>
@@ -34,37 +22,54 @@ const Blog = () => {
               officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
               amet.
             </p>
-          </div>
-          <hr className={classes.under_line} />
-          <div className={classes.item}>
-            <h2 className={classes.item_header}>UI Interactions of the week</h2>
-            <div className={classes.item_header_description}>
-              <p className={classes.item_header_description_date}>12 Feb 2019</p>
-              <p className={classes.item_header_description_separator}>|</p>
-              <p className={classes.item_header_description_name}>Express, Handlebars</p>
-            </div>
-            <p className={classes.item_description_main_text}>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-              officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
-              amet.
-            </p>
-          </div>
-          <hr className={classes.under_line} />
-          <div className={classes.item}>
-            <h2 className={classes.item_header}>UI Interactions of the week</h2>
-            <div className={classes.item_header_description}>
-              <p className={classes.item_header_description_date}>12 Feb 2019</p>
-              <p className={classes.item_header_description_separator}>|</p>
-              <p className={classes.item_header_description_name}>Express, Handlebars</p>
-            </div>
-            <p className={classes.item_description_main_text}>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-              officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
-              amet.
-            </p>
-          </div>
-          <hr className={classes.under_line} />
+          </article>
         </div>
+        <hr className={classes.under_line} />
+        <div className={classes.item}>
+          <header className={classes.item_header}>
+            <h2 className={classes.item_header_text}>UI Interactions of the week</h2>
+          </header>
+          <div className={classes.item_header_description}>
+            <p className={classes.item_header_description_date}>12 Feb 2019</p>
+            <p className={classes.item_header_description_separator}>|</p>
+            <p className={classes.item_header_description_name}>Express, Handlebars</p>
+          </div>
+          <p className={classes.item_description_main_text}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <hr className={classes.under_line} />
+        <div className={classes.item}>
+          <header className={classes.item_header}>
+            <h2 className={classes.item_header_text}>UI Interactions of the week</h2>
+          </header>
+          <div className={classes.item_header_description}>
+            <p className={classes.item_header_description_date}>12 Feb 2019</p>
+            <p className={classes.item_header_description_separator}>|</p>
+            <p className={classes.item_header_description_name}>Express, Handlebars</p>
+          </div>
+          <p className={classes.item_description_main_text}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <hr className={classes.under_line} />
+        <div className={classes.item}>
+          <header className={classes.item_header}>
+            <h2 className={classes.item_header_text}>UI Interactions of the week</h2>
+          </header>
+          <div className={classes.item_header_description}>
+            <p className={classes.item_header_description_date}>12 Feb 2019</p>
+            <p className={classes.item_header_description_separator}>|</p>
+            <p className={classes.item_header_description_name}>Express, Handlebars</p>
+          </div>
+          <p className={classes.item_description_main_text}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <hr className={classes.under_line} />
       </div>
     </section>
   )
--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Blog from '../../entities/Blog/ui/Blog'
 
 const BlogPage = () => {
-  return <Blog />
+  return (
+    <main>
+      <Blog />
+    </main>
+  )
 }
 
 export default BlogPage
