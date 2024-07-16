4d27c6d968661b87a52dfc4ddb611f41953c2ac5
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-07-16 20:38:26 +0300

    feat: Добавил перемещение скролла при переходе
    - при переходе на новую страницу, скролл поднимается наверх

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import PortfolioUI from './pages/PortfolioUI'
+import ScrollToTop from './shared/scrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <main>
         <PortfolioUI />
       </main>
